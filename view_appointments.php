57d2855c705e6fb151d3a0ef4d0cbcfaa5d7669d
Sanuka Thamuditha <158992222+Sanuka23@users.noreply.github.com>
2024-09-13 00:21:52 +0530

    fix

--- view_appointments.php
+++ view_appointments.php
@@ -9,7 +9,6 @@ if (!isset($_SESSION['doctor_id'])) {
 }
 
 $doctor_id = $_SESSION['doctor_id']; // Get doctor ID from session
-
 ?>
 
 <!doctype html>
@@ -69,7 +68,7 @@ $doctor_id = $_SESSION['doctor_id']; // Get doctor ID from session
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6Hty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-zMpOr0vL9TzNu1jzG+p4AqkBdhavRrWIe0iDlEu5KQ/xNUolGpP0xKqRk9b1nx+3" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
     <nav class="navbar navbar-expand-sm navbar-dark" style="background-color: black;">
